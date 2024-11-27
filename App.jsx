9d065940ff440a320747683de3923b16938f80e5
Abhishek Pawar <abhipawarraipur@gmail.com>
2024-11-26 18:48:07 -0800

    authUiCreate

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AuthenticationScreen from './src/screen/auth';
+
+const App = () => {
+  return(
+    <>
+    <AuthenticationScreen/>
+    </>
+  )
+}
+
+export default App;
